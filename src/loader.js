46a6c36e342de7bd2315fa3abf6b64d2bf98eb11
Marek Cermak <macermak@redhat.com>
2019-08-22 11:48:03 +0200

    Loader: Initialize Trello before loading plugins
    Loader does not take the API_KEY anymore, intead the trello client
    should be loaded from the injector directly.
    
    Signed-off-by: Marek Cermak <macermak@redhat.com>
    
    modified:   loader.js

--- src/loader.js
+++ src/loader.js
@@ -1,72 +1,69 @@
 /**
- *  This is a Trello JS "plugin" loader.
- *  
- *  Usage:
- *    Provide API key to the Trello account as global variable `APIKEY`
- *    Attach links to the plugins you wanna fetch and load.
+  *  This is a Trello JS "plugin" loader.
+  *  
+  *  Usage: Attach links to the plugins you wanna fetch and load.
  */
 
-
-// APIKEY = ...
-
 const plugins = [
-	`https://trello.com/1/client.js?key=${APIKEY}`,
-	"https://gist.githubusercontent.com/CermakM/e51c2fead4e97e9bf341afdad62aac18/raw/e990b4ac5990d6c863ad3ee1abc8a330acf90e78/trello-sort.js"
+  "https://gist.githubusercontent.com/CermakM/e51c2fead4e97e9bf341afdad62aac18/raw/e990b4ac5990d6c863ad3ee1abc8a330acf90e78/trello-sort.js",
+  "https://raw.githubusercontent.com/CermakM/trello-utils/master/src/plugins/sp/counter.js"
 ]
 const scripts = []
 
+
 $('.trello-root').ready(function () { 
-	plugins.forEach( p => {
-		const script = $('<script/>', {
-			type: "text/javascript",
-			crossorigin: 'anonymous',
-			class : "trello-script"
-		})
+  
+  /**
+   *  Trello authentication
+   */
 
-		fetch(p)
-			.then( r => r.text())
-			.then( t => script.text(t))
-			.then( $('body').append(script) )
-			.catch(console.error)
-	})
+  function authenticationSuccess() {
+    console.warn('Successful authentication')
+  }
+  
+  function authenticationFailure() {
+    console.warn('Failed authentication')
+  }
 
-	/**
-	 *  Trello authentication
-	 */
-
-	function authenticationSuccess() {
-		console.warn('Successful authentication')
-	}
-
-	function authenticationFailure() {
-		console.warn('Failed authentication')
-	}
-
-	setTimeout( async () => {
-		if (Trello.authorized()) {
-			console.warn("Trello has already been authenticated.")
-		}
-
-		Trello.authorize({
-			type: 'popup',
-			name: 'Trello Utils',
-			scope: {
-				read: 'true',
-				write: 'true'
-			},
-			persist: true,
-			expiration: 'never',
-			success: authenticationSuccess,
-			error: authenticationFailure
-		})
-
-		Trello.board = {
-			get: async () => {
-				Object.assign(Trello.board, await $.get(document.URL + ".json"))
-
-				return Trello.board
-			}
-		}
-
-	}, 200)
+  setTimeout( async () => {
+    if (Trello.authorized()) {
+      console.warn("Trello has already been authenticated.")
+    }
+    
+    Trello.authorize({
+      type: 'popup',
+      name: 'Trello Utils',
+      scope: {
+        read: 'true',
+        write: 'true'
+      },
+      persist: true,
+      expiration: 'never',
+      success: authenticationSuccess,
+      error: authenticationFailure
+    })
+    
+    Trello.board = {
+      get: async () => {
+        Object.assign(Trello.board, await $.get(document.URL + ".json"))
+        
+        return Trello.board
+      }
+    }
+   
+    plugins.forEach( p => {
+      const script = $('<script/>', {
+        type: "text/javascript",
+        crossorigin: 'anonymous',
+        class : "trello-script"
+      })
+      
+      fetch(p)
+        .then( r => r.text())
+        .then( t => script.text(t))
+        .then( $('body').append(script) )
+        .catch(console.error)
+    })
+  }, 200)
 })
+
